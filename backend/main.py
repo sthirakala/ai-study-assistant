c91e3e06be5f423079300e865a98dedd94bbeef0
sthirakala <sthirakala@umass.edu>
2026-09-16 21:10:27 -0400

    Allow frontend port 5174

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,10 @@ llm = ChatGroq(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:5174"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
